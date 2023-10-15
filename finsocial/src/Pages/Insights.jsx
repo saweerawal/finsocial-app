ff53d1220487678164a597c287ccebc54a5c856d
Sawee Rawal <38999843+saweerawal@users.noreply.github.com>
2023-10-16 01:54:38 +0530

    Insights topbar feature

--- finsocial/src/Pages/Insights.jsx
+++ finsocial/src/Pages/Insights.jsx
@@ -1,6 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useMemo } from "react";
+import Topbar from "../components/common/Topbar";
+import { getCurrentUser } from "../api/FirestoreAPI";
 import InsightsComponent from "../components/InsightsComponent";
 
-export default function StockNews() {
-    return <InsightsComponent />;
+export default function Insights() {
+    const [currentUser, setCurrentUser] = useState({});
+
+    useMemo(() => {
+        getCurrentUser(setCurrentUser);
+      }, []);
+    return (<div>
+    <Topbar currentUser={currentUser} />
+    <InsightsComponent />
+  </div>);
 }
