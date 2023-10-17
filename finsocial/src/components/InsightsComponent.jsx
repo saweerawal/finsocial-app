73fb94b69d27320c3b6af78a7e9b63d4c1d452c8
Paramdubey <paramdubey97@gmail.com>
2023-10-18 00:09:02 +0530

    insightscss

--- finsocial/src/components/InsightsComponent.jsx
+++ finsocial/src/components/InsightsComponent.jsx
@@ -1,57 +1,61 @@
 import React, { useEffect, useState } from "react";
-/*import "../Sass/InsightsComponent.scss";*/
-
-import ReactSpeedometer from "react-d3-speedometer/slim"
-// and just use it
+import "./InsightsComponent.css";  // Import your CSS styles
+import ReactSpeedometer from "react-d3-speedometer/slim";
 
 export default function InsightsComponent() {
   return (
-    <div className="">
-    <ReactSpeedometer
-    maxValue={999} 
-    value={900}
-    currentValueText="Sentiment Analysis"
-    segments={3}
-    customSegmentLabels={[
-      {
-        text: "Sell",
-        position: "INSIDE",
-        color: "#555",
-      },
-      {
-        text: "Hold",
-        position: "INSIDE",
-        color: "#555",
-      },
-      {
-        text: "Buy",
-        position: "INSIDE",
-        color: "#555",
-      },
-    ]} />
+    <div className="insights-container">
+      <div className="speedometer">
+        <ReactSpeedometer
+          maxValue={999}
+          value={360}
+          currentValueText="Sentiment Analysis"
+          segments={3}
+          customSegmentLabels={[
+            {
+              text: "Sell",
+              position: "INSIDE",
+              color: "#555",
+            },
+            {
+              text: "Hold",
+              position: "INSIDE",
+              color: "#555",
+            },
+            {
+              text: "Buy",
+              position: "INSIDE",
+              color: "#555",
+            },
+          ]}
+        />
+      </div>
 
-<ReactSpeedometer
-    maxValue={999} 
-    value={900}
-    currentValueText="Market Analysis"
-    segments={3}
-    customSegmentLabels={[
-      {
-        text: "Sell",
-        position: "INSIDE",
-        color: "#555",
-      },
-      {
-        text: "Hold",
-        position: "INSIDE",
-        color: "#555",
-      },
-      {
-        text: "Buy",
-        position: "INSIDE",
-        color: "#555",
-      },
-    ]} />
+      <div className="speedometer">
+        <ReactSpeedometer
+          maxValue={999}
+          value={900}
+          currentValueText="Market Analysis"
+          segments={3}
+          customSegmentLabels={[
+            {
+              text: "Sell",
+              position: "INSIDE",
+              color: "#555",
+            },
+            {
+              text: "Hold",
+              position: "INSIDE",
+              color: "#555",
+            },
+            {
+              text: "Buy",
+              position: "INSIDE",
+              color: "#555",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 }
